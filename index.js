aa79414c8fb94036a413e05d7b4138299cff3387
Mark Keith <mark.r.keith@gmail.com>
2016-04-04 12:32:14 -0700

    Setup index.js and packaje.json for Heroku hosting.

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+// To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-
-var app = express();
-
-app.get('/', function(req, res) {
-  console.log('Hey creator. I\'m alive at port 3000');
-  res.send('Hello World');
-}).listen(3000);
