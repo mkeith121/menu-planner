052513d96a32bb7a7a62b8bd3526fded8b22a2c4
Mark Keith <mark.r.keith@gmail.com>
2016-04-05 13:16:14 -0700

    App working with functional table, service and controller

--- public/entryController.js
+++ public/entryController.js
@@ -1,9 +1,54 @@
-app.controller('entryController', function($scope) {
-  $scope.entry = {};
-  $scope.entries = [];
-  $scope.addEntry = function(entry) {
-    var myEntry = angular.copy(entry);
-    $scope.entries.push(myEntry);
-    $scope.entry = {};
+angular.module('menuApp.otherModule', [])
+.service('entryCreator', function() {
+  this.dayEntry = {
+    day: '',
+    entry: {
+      Breakfast: ' ',
+      Lunch: ' ',
+      Dinner: ' ',
+      Snack:' '
+    }
   };
+  this.dayEntries = [];
+
+  this.getDayEntry = function(entry) {
+    var exists = false;
+    if(this.dayEntries.length > 0) {
+      this.dayEntries.forEach(function(item) {
+        if(!exists) {
+          if(item.day === entry.day) {
+            exists = item;
+          } 
+        }
+      });
+    }
+    return exists;
+  };
+
+  this.updateEntry = function(entry, callback) {
+    if(this.getDayEntry(entry)) {
+      myEntry.entry[entry.meal] = entry.input;
+    } else {
+      myEntry = angular.copy(this.dayEntry);
+      myEntry.day = entry.day;
+      myEntry.entry[entry.meal] = entry.input;
+      this.dayEntries.push(myEntry);
+    }
+    callback(this.dayEntries);
+  };
+
 })
+
+
+
+.controller('entryController', ['$scope', 'entryCreator', function($scope, entryCreator) {
+    $scope.entry = {};
+    $scope.entries = [];
+    $scope.addEntry = function(entry) {
+      entryCreator.updateEntry(entry, function(collection) {
+        $scope.entries = collection;
+      })
+    };
+  }
+]);
+
--- public/menuApp.js
+++ public/menuApp.js
@@ -1,1 +1,2 @@
-var app = angular.module('menuApp', []);
+angular.module('menuApp', [
+  'menuApp.otherModule']);
